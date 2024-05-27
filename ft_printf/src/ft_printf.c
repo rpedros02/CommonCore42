2ae4da302d887ae9a87f967a64138a4fc8c23b56
Rui <ruicorreias02@gmail.com>
2024-05-28 00:40:10 +0200

    printf updated (lacking critical functions)

--- ft_printf/src/ft_printf.c
+++ ft_printf/src/ft_printf.c
@@ -6,16 +6,61 @@
 /*   By: rucorrei <rucorrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 12:02:10 by rucorrei          #+#    #+#             */
-/*   Updated: 2024/05/24 12:10:45 by rucorrei         ###   ########.fr       */
+/*   Updated: 2024/05/28 00:35:07 by rucorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_printf.h>
+#include "libft.h"
+
+int ft_printchar(char c)
+{
+	ft_putchar(c);
+	return (1);
+}
+
+int	ft_parse(const char c, va_list args)
+{
+	int	len;
+	
+	len = 0;
+	if (c == 'c')
+		len += ft_printchar(va_arg(args, int));
+	if (c == 's')
+		return (ft_printstr(va_arg(args, char *)));
+	if (c == 'p')
+		return (ft_putptr(va_arg(args, void *)));
+	if (c == 'd' || c == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	if (c == 'u')
+		return (ft_putunbr(va_arg(args, unsigned int)));
+	if (c == 'x')
+		return (ft_puthex(va_arg(args, unsigned int), 0));
+	if (c == 'X')
+		return (ft_puthex(va_arg(args, unsigned int), 1));
+	if (c == '%')
+		return (ft_putchar('%'));
+	return (0);
+}
 
 int	ft_printf(const char *str, ...){
 	va_list	args;
+	int		i;
+	int		len;
 	
 	va_start(args, str);
-	
+	i = 0;
+	len = 0;
+	while (str[i] != '\0'){
+		if (str[i] == '%'){
+			i++;
+			len += ft_parse(&str[i], args);
+		}
+		else{
+			ft_putchar(str[i]);
+			len++;
+		}
+		i++;
+	}
 	va_end(args);
 }
---
+++ ft_printf/src/ft_printf_utils.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rucorrei <rucorrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 00:36:58 by rucorrei          #+#    #+#             */
+/*   Updated: 2024/05/28 00:37:44 by rucorrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_printf.h>
+#include "libft.h"
+
+int	ft_printstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str == NULL)
+	{
+		ft_putstr("(null)");
+		return (6);
+	}
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
